6f70109d6d30e117a73dc95c86419f0e55d5b122
vasanthkumar <vasanthkumar5151@gmail.com>
2026-04-17 12:11:15 +0530

    db timeout error

--- server/lib/db.js
+++ server/lib/db.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 
-const dbconnect = async () =>{
+const dbconnect = async () => {
+    if (mongoose.connection.readyState >= 1) return;
+
     try {
-       await mongoose.connect(`${process.env.MONGODB_URI}`)
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log("db connected");
-        
     } catch (error) {
-        console.log(error);
-           
+        console.error("MongoDB connection error:", error);
     }
 }
 
