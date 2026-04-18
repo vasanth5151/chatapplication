1587869d2d2f5cbf936923613e33a2c5af80797b
vasanthkumar <vasanthkumar5151@gmail.com>
2026-04-18 11:53:32 +0530

    update db js

--- server/lib/db.js
+++ server/lib/db.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const dbconnect = async () => {
     try {
-
+        if (!process.env.MONGODB_URI) {
+            console.error("FATAL ERROR: MONGODB_URI is undefined! Did you forget to add Environment Variables in Render?");
+            process.exit(1);
+        }
         await mongoose.connect(process.env.MONGODB_URI);
         console.log("db connected");
     } catch (error) {
